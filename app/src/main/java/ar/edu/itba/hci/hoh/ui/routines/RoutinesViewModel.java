f19099889d16125296d764e36bfc84d7dea3054a
julian <jtallar@itba.edu.ar>
2019-11-06 14:43:04 -0300

    Added fragment

--- app/src/main/java/ar/edu/itba/hci/hoh/ui/routines/RoutinesViewModel.java
+++ app/src/main/java/ar/edu/itba/hci/hoh/ui/routines/RoutinesViewModel.java
@@ -10,7 +10,7 @@ public class RoutinesViewModel extends ViewModel {
 
     public RoutinesViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is routines fragment");
     }
 
     public LiveData<String> getText() {
